ddaa5bfe118d05f16f7bc2e1dcd3a56d2e0745eb
roliveros-ramos <ricardo.oliveros@gmail.com>
2018-02-01 17:37:42 +0100

    bugs corrected in regrid

--- R/nctools-regrid.R
+++ R/nctools-regrid.R
@@ -18,19 +18,28 @@
 #'
 #' @examples
 nc_regrid = function(filename, varid, dim, new, mask=NULL, output, fill=FALSE,
-                     radius=1) {
+                     radius=1, log=TRUE, ...) {
 
   nc =  nc_open(filename)
-  x = ncvar_get(nc, varid, collapse_degen = FALSE)
+
   old = list(lon=ncvar_get(nc, dim[1]),
              lat=ncvar_get(nc, dim[2]))
 
-  old$lon = kali::checkLongitude(old$lon)
-  new$lon = kali::checkLongitude(new$lon)
+  new$lon = new[[dim[1]]]
+  new$lat = new[[dim[2]]]
 
-  x = kali::regrid(object=x, old=old, new=new, mask=mask)
-  if(isTRUE(fill))
-    x = kali::fillMap(object=x, mask=new$mask, radius=radius, fill.value=NA)
+  new$lon = checkLongitude(new$lon,
+                           primeMeridian = findPrimeMeridian(old$lon),
+                           sort=FALSE)
+
+  x = ncvar_get(nc, varid, collapse_degen = FALSE)
+  if(isTRUE(log)) x = log(x + 1e-4)
+
+  if(!isTRUE(fill)) {
+    x = kali::regrid(object=x, old=old, new=new, mask=mask)
+  } else {
+    x = kali::regrid2(object=x, old=old, new=new, mask=mask, ...)
+  }
 
   newVar = nc$var[[varid]]
   newVar$size = dim(x)
@@ -46,12 +55,217 @@ nc_regrid = function(filename, varid, dim, new, mask=NULL, output, fill=FALSE,
 
   newVar$dim = lapply(names(nc$dim), FUN=.modifyDim, dim=nc$dim, index=new)
 
+  if(!dir.exists(dirname(output)))
+    dir.create(dirname(output), recursive = TRUE)
+
   ncNew = nc_create(filename=output, vars=newVar)
+
+  if(isTRUE(log)) x = exp(x) - 1e-4
   ncvar_put(ncNew, varid, x)
   nc_close(ncNew)
 
   nc_close(nc)
 
   return(invisible(output))
+}
+
+
+
+
+#' Title
+#'
+#' @param filename
+#' @param varid
+#' @param dim
+#' @param new
+#' @param mask
+#' @param output
+#' @param fill
+#' @param radius
+#' @param log
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nc_regrid2 = function(filename, varid=NULL, dim, new, mask=NULL, output, fill=FALSE,
+                     radius=1, log=TRUE, ...) {
+
+  nc =  nc_open(filename)
+
+  old = list(lon=ncvar_get(nc, dim[1]),
+             lat=ncvar_get(nc, dim[2]))
+
+  new$lon = checkLongitude(new$lon,
+                           primeMeridian = findPrimeMeridian(old$lon),
+                           sort=TRUE)
+
+  .modifyDim = function(x, dim, index) {
+    if(is.null(index[[x]])) return(dim[[x]])
+    dim[[x]]$size = length(index[[x]])
+    dim[[x]]$len = length(index[[x]])
+    dim[[x]]$vals = index[[x]]
+    return(dim[[x]])
+  }
+
+  newVars = list()
+  vars = if(is.null(varid)) names(nc$var) else varid
+
+  for(varid in vars) {
+    newVar = nc$var[[varid]]
+    newVar$size[1:2] = c(length(new$lon), length(new$lat))
+    newVar$chunksizes = NA
+    newVar$dim = lapply(names(nc$dim), FUN=.modifyDim, dim=nc$dim, index=new)
+    newVars[[varid]] = newVar
+  }
+
+  ncNew = nc_create(filename=output, vars=newVars)
+
+  for(varid in vars) {
+
+    x = ncvar_get(nc, varid, collapse_degen = FALSE)
+    if(isTRUE(log)) x = log(x + 1e-4)
+
+    if(!isTRUE(fill)) {
+      x = regrid(object=x, old=old, new=new, mask=mask)
+    } else {
+      x = regrid2(object=x, old=old, new=new, mask=mask, ...)
+    }
+
+    if(isTRUE(log)) x = exp(x) - 1e-4
+    ncvar_put(ncNew, varid, x)
+
+  }
+
+  nc_close(ncNew)
+  nc_close(nc)
+
+  return(invisible(output))
+}
+
+
+
+# regrid ------------------------------------------------------------------
+
+
+regrid = function(object, old, new, mask, ...) {
+  UseMethod("regrid")
+}
+
+regrid.matrix = function(object, old, new, mask=NULL, ...) {
+
+  if(is.null(mask)&!is.null(new$mask)) mask=new$mask
+  stopifnot(exists("lat", where=old), exists("lon", where=old))
+  stopifnot(length(old$lat)==ncol(object), length(old$lon)==nrow(object))
+
+  stopifnot(exists("lat", where=new), exists("lon", where=new))
+  stopifnot(is.numeric(new$lat), !is.matrix(new$lat),
+            is.numeric(new$lon), !is.matrix(new$lon))
+
+  old$x = old$lon
+  old$y = old$lat
+  old$z = object
+
+  new$x = new$lon
+  new$y = new$lat
+
+  newp = interp.surface.grid(obj=old, grid.list=new, ...)$z
+  newmap = if(!is.null(mask)) newp*mask else newp
+
+  return(newmap)
+}
+
+
+regrid.array = function(object, old, new, mask=NULL, ...) {
+  # new grid
+  if(exists("LAT", where=new) & exists("LON", where=new)) {
+    stopifnot(is.matrix(new$LAT), is.matrix(new$LON), dim(new$LAT)==dim(new$LON))
+    nLAT = new$LAT
+    nLON = new$LON
+    nlat = as.numeric(nLAT)
+    nlon = as.numeric(nLON)
+  } else {
+    if(exists("lat", where=new) & exists("lon", where=new)) {
+      stopifnot(is.numeric(lat), !is.matrix(lat), is.numeric(lon), !is.matrix(lon))
+      nLAT = matrix(new$lat, ncol=length(new$lat), nrow=length(new$lon), byrow=TRUE)
+      nLON = matrix(new$lon, ncol=length(new$lat), nrow=length(new$lon))
+      nlat = as.numeric(nLAT)
+      nlon = as.numeric(nLON)
+    } else {
+      stop("'new' must contain latitude and longitude information.")
+    }
+  }
+
+  ndim = seq_along(dim(object))[-c(1,2)]
+  newmap = apply(object, ndim, regrid, old=old, new=new, mask=mask, ...)
+  dim(newmap) = c(dim(nLAT), dim(object)[-c(1,2)])
+  return(newmap)
+}
+
+
+# regrid2 -----------------------------------------------------------------
+
+
+
+regrid2 = function(object, old, new, mask, linear=TRUE, extrap=FALSE, ...) {
+  UseMethod("regrid2")
+}
+
+regrid2.matrix = function(object, old, new, mask=NULL, linear, extrap, ...) {
+
+  if(is.null(mask)&!is.null(new$mask)) mask=new$mask
+  stopifnot(exists("lat", where=old), exists("lon", where=old))
+  stopifnot(length(old$lat)==ncol(object), length(old$lon)==nrow(object))
+
+  stopifnot(exists("lat", where=new), exists("lon", where=new))
+  stopifnot(is.numeric(new$lat), !is.matrix(new$lat),
+            is.numeric(new$lon), !is.matrix(new$lon))
+
+  old$x = rep(old$lon, ncol(object))
+  old$y = rep(old$lat, each=nrow(object))
+  old$z = as.numeric(object)
+
+  old = old[c("x","y","z")]
+
+  old = as.data.frame(old)
+  old = old[complete.cases(old), ]
+
+  new$x = new$lon
+  new$y = new$lat
+
+  newp = interp(x=old$x, y=old$y, z=old$z, xo=new$x, yo=new$y,
+                linear=linear, extrap=extrap, ...)$z
+  newmap = if(!is.null(mask)) newp*mask else newp
+
+  return(newmap)
+}
+
+
+regrid2.array = function(object, old, new, mask=NULL, linear, extrap, ...) {
+  # new grid
+  if(exists("LAT", where=new) & exists("LON", where=new)) {
+    stopifnot(is.matrix(new$LAT), is.matrix(new$LON), dim(new$LAT)==dim(new$LON))
+    nLAT = new$LAT
+    nLON = new$LON
+    nlat = as.numeric(nLAT)
+    nlon = as.numeric(nLON)
+  } else {
+    if(exists("lat", where=new) & exists("lon", where=new)) {
+      stopifnot(is.numeric(lat), !is.matrix(lat), is.numeric(lon), !is.matrix(lon))
+      nLAT = matrix(new$lat, ncol=length(new$lat), nrow=length(new$lon), byrow=TRUE)
+      nLON = matrix(new$lon, ncol=length(new$lat), nrow=length(new$lon))
+      nlat = as.numeric(nLAT)
+      nlon = as.numeric(nLON)
+    } else {
+      stop("'new' must contain latitude and longitude information.")
+    }
+  }
+
+  ndim = seq_along(dim(object))[-c(1,2)]
+  newmap = apply(object, ndim, regrid2, old=old, new=new, mask=mask,
+                 linear=linear, extrap=extrap, ...)
+  dim(newmap) = c(dim(nLAT), dim(object)[-c(1,2)])
+  return(newmap)
 }
 
