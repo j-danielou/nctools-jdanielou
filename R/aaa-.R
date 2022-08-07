65fd79270f855ac064386708d226ab1ba2c055a9
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2022-08-07 21:22:54 +0200

    imports from ncdf4

--- R/aaa-.R
+++ R/aaa-.R
@@ -1,5 +1,7 @@
 #' @importFrom stats quantile median sd
 NULL
+#' @importFrom ncdf4 ncdim_def ncvar_def nc_create ncvar_put nc_close ncatt_put
+NULL
 
 # #' @importFrom akima interp
 # #' @importFrom fields interp.surface.grid
